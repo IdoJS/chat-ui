7eba7526e4c152e994baecd8a0cb66c999533e73
idokatz <Idok.mail@gmail.com>
2018-11-20 23:09:55 +0200

    Chat room - socketObserver

--- src/containers/ChatRoom/index.js
+++ src/containers/ChatRoom/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import socketObserver from '../../utils/socketObserver';
 
 const ChatRoom = () => {
+  socketObserver.subscribe({identifier : '', o : function (data) {
+    console.log('socket', data);
+  }});
+
+  socketObserver.send({
+    data : 'test'
+  });
   return <div>Chat room</div>
 };
 
---
+++ src/utils/socketObserver.js
@@ -0,0 +1,46 @@
+import io from 'socket.io-client';
+
+const SOCKET_URL = 'https://spotim-demo-chat-server.herokuapp.com';
+const SOCKET_EVENT = 'spotim/chat';
+/**
+ * Singleton Observer that listen to socket
+ */
+class SocketObserver {
+
+  static instance;
+
+  constructor(){
+    if(this.instance){
+      return this.instance;
+    }
+
+    this.instance = this;
+
+    this.socket = io(SOCKET_URL);
+    this.observers = [];
+
+
+    this.socket.on(SOCKET_EVENT, (response) => {
+      const {data} = response;
+      this.observers.forEach(obs => obs.o(data));
+    });
+  }
+
+  subscribe({identifier, o}){
+    this.observers.push({identifier, o});
+    return this;
+  }
+
+  unSubscribe({identifier}){
+    this.observers = this.observers.filter(ob => ob.identifier !== identifier);
+    return this;
+  }
+
+  send(requestData){
+    this.socket.emit(SOCKET_EVENT, requestData);
+    return this;
+  }
+
+}
+
+export default new SocketObserver();
